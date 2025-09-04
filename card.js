adf07c41deac03ace058fd42dc7ea294706f06a4
Anik Ghosh <anikgh7@gmail.com>
2025-09-04 16:58:14 +0600

    copy and call button functionality

---
+++ card.js
@@ -0,0 +1,54 @@
+let cardCount = 0
+let copyCount = 0
+function card() {
+    document.getElementById('card-items').addEventListener('click', function (e) {
+        if (e.target.tagName == 'BUTTON') {
+            const button = e.target;
+            //    const callBtn= button.parentNode.parentNode.children[5].children[1].innerText; 
+            const coin = document.getElementById('coin').innerText
+
+
+            const callBtn = e.target.innerText.trim()
+            if (callBtn == 'Call') {
+
+                const call = button.parentNode.parentNode.children[3].innerText
+                if (call) {
+                    alert(document.createElement('div')
+                    )
+                    cardCount++  //Number(call) for add the value
+                    const callCount = document.getElementById('call-count').innerText = cardCount
+                }
+
+
+            }
+            else if (callBtn == 'Copy') {
+                copyCount++
+                document.getElementById('copy').innerText = copyCount
+
+            }
+            console.log(callBtn)
+        }
+    })
+}
+card()
+
+
+// for copy   button functionality
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    const copyBtns = document.querySelectorAll('.copy-btn'); 
+    // select all copy buttons
+
+    copyBtns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            const card = btn.closest('.card-item'); 
+            // find the closest card
+            const textEl = card.querySelector('.copy-text'); 
+            // get the number
+            const text = textEl.innerText.trim();
+
+            // Copy to clipboard
+            navigator.clipboard.writeText(text)
+        });
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['"Hind Madurai"', 'sans-serif'],
+
+      },
+       boxShadow: {
+        custom: "0px 2px 3px 0px rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+}
